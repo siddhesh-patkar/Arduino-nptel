a9c1581f7bc465a769e4a3be572f92e9e02fe55f
Siddhesh Patkar <patkarsiddhesh2018@gmail.com>
2021-02-09 08:04:41 +0530

    added modified

--- tricolor-led/tricolor-led.ino
+++ tricolor-led/tricolor-led.ino
@@ -1,29 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+vvoid setup()
+{
   pinMode(10,OUTPUT);//Blue
   pinMode(11,OUTPUT);//Red
   pinMode(9,OUTPUT);//Green
-
+  pinMode(4,INPUT);//pushButton
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(10,HIGH);//Blue
-  delay(500);
-  digitalWrite(10,LOW);
-  delay(500);
-  
-  
-  digitalWrite(9,HIGH);//Green
-  delay(500);
-  digitalWrite(9,LOW);
-  delay(500);
-  
-  
-  digitalWrite(11,HIGH);//Red
-  delay(500);
-  digitalWrite(11,LOW);
-  delay(500);
-  
+void loop()
+{
+  if(digitalRead(4) == HIGH)
+  {
+    digitalWrite(10,HIGH);//Blue
+    delay(100);
+    digitalWrite(10,LOW);
+    delay(100);
 
+
+    digitalWrite(9,HIGH);//Green
+    delay(100);
+    digitalWrite(9,LOW);
+    delay(100);
+
+
+    digitalWrite(11,HIGH);//Red
+    delay(100);
+    digitalWrite(11,LOW);
+    delay(100);
+  }
+  
 }
